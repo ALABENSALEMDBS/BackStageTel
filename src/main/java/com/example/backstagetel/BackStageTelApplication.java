06a3fbc07447779a6779604a0666931c38e98ca5
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-07-14 11:59:37 +0100

    test push

---
+++ src/main/java/com/example/backstagetel/BackStageTelApplication.java
@@ -0,0 +1,13 @@
+package com.example.backstagetel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStageTelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStageTelApplication.class, args);
+	}
+
+}
